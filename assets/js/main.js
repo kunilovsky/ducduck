6987aa07dbc611a5be7b1dce042a8cace377c335
kunilovsky <kunilovsky@gmail.com>
2023-05-27 16:13:58 +0300

    Section start

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//Copy e-mail
+let text = document.getElementById('mail-data').innerHTML;
+const copyContent = async () => {
+  try {
+    await navigator.clipboard.writeText(text);
+  } catch (err) {}
+};
+
+//Open nav modal
+const navBtnModal = document.getElementById('nav-modal');
+const navModal = document.getElementById('modal-top-nav');
+const headerColor = document.getElementById('header');
+const navbarLeft = document.getElementById('navbar-nav__left');
+const navItem = document.querySelectorAll('.nav-link');
+const logo = document.getElementById('logo');
+const logoDuck = document.getElementById('logo-duck');
+
+navBtnModal.addEventListener('click', () => {
+  navBtnModal.classList.toggle('nav-modal-open');
+  navModal.classList.toggle('modal-top-nav-active');
+  headerColor.classList.toggle('header-yellow');
+  navbarLeft.classList.toggle('navbar-nav__left-hide');
+  navItem.forEach((el) => {
+    el.classList.toggle('nav-link-black');
+  });
+  logo.classList.toggle('navbar-brand-black');
+  logoDuck.classList.toggle('logo-dark');
+});
