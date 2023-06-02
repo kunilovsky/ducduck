ac088fea208d243a7c47ca3da4fccd994d34afe7
kunilovsky <kunilovsky@gmail.com>
2023-06-02 13:21:18 +0300

    Section slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,67 @@ navBtnModal.addEventListener('click', () => {
   logo.classList.toggle('navbar-brand-black');
   logoDuck.classList.toggle('logo-dark');
 });
+
+//Slide lib
+const slideBtnNext = document.getElementById('btn-s-next');
+const slideBtnBack = document.getElementById('btn-s-back');
+const slideItem = document.querySelectorAll('.slide-item');
+const slidePosiiton = document.getElementById('slide-position-item');
+let units = slidePosiiton.value.replace(/\d/g, '');
+let currentIndex = 0;
+
+slideBtnNext.addEventListener('click', () => {
+  if (currentIndex < slideItem.length - 1) {
+    slideItem[currentIndex].classList.remove('slide-item-active');
+    currentIndex++;
+    slideItem[currentIndex].classList.add('slide-item-active');
+    slidePosiiton.value = parseInt(slidePosiiton.value) + 1 + units;
+  }
+});
+
+slideBtnBack.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    slideItem[currentIndex].classList.remove('slide-item-active');
+    currentIndex--;
+    slideItem[currentIndex].classList.add('slide-item-active');
+    slidePosiiton.value = parseInt(slidePosiiton.value) - 1 - units;
+  }
+});
+
+//Slide hirring
+const slideBtnNextHirring = document.getElementById('btn-s-next-hirring');
+const slideBtnBackHirring = document.getElementById('btn-s-back-hirring');
+const slideItemHirring = document.querySelectorAll('.slide-item-hirring');
+const slidePosiitonHirring = document.getElementById(
+  'slide-position-item-hirring'
+);
+let unitsHirring = slidePosiitonHirring.value.replace(/\d/g, '');
+let currentIndexHirring = 0;
+
+slideBtnNextHirring.addEventListener('click', () => {
+  if (currentIndexHirring < slideItemHirring.length - 1) {
+    slideItemHirring[currentIndexHirring].classList.remove(
+      'slide-item-hirring-active'
+    );
+    currentIndexHirring++;
+    slideItemHirring[currentIndexHirring].classList.add(
+      'slide-item-hirring-active'
+    );
+    slidePosiitonHirring.value =
+      parseInt(slidePosiitonHirring.value) + 1 + unitsHirring;
+  }
+});
+
+slideBtnBackHirring.addEventListener('click', () => {
+  if (currentIndexHirring > 0) {
+    slideItemHirring[currentIndexHirring].classList.remove(
+      'slide-item-hirring-active'
+    );
+    currentIndexHirring--;
+    slideItemHirring[currentIndexHirring].classList.add(
+      'slide-item-hirring-active'
+    );
+    slidePosiitonHirring.value =
+      parseInt(slidePosiitonHirring.value) - 1 - unitsHirring;
+  }
+});
